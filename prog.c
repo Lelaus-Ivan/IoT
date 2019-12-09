62f4c342255545598bf07c6e54c63333a43ddc2e
Lelaus-Ivan <53258091+Lelaus-Ivan@users.noreply.github.com>
2019-12-09 17:07:14 +0500

    09.12

--- prog.c
+++ prog.c
@@ -4,23 +4,33 @@
 int main()
 {
 char str[128];
-float b=0;
+float b;
   while (scanf("%s" , str))
 	{
-	if (!strcmp(str, "\'temperature\':"))
+
+	if (!strcmp(str, "\"temperature\":"))
 		{
 		scanf("%f", &b);
+		printf("%f \n",b);
 		}
-	if (b>= 10,0)
-		{
-		system ("sudo mosquito_pub -h \"10.96.4.11\" -t\"devices/lora/807B85902000069B/gpio\" -m \"gpio set 1 1\" -q 1");
-	}
-	else
+	if (b<0)
 	{
-	system ("sudo mosquito_pub -h \"10.96.4.11\" -t\"devices/lora/807B85902000069B/gpio\" -m \"gpio set 1 0\" -q 1");
 	}
+
+		else
+		{
+			if (b>= 10,0)
+			{
+		system ("mosquitto_pub -h  \"10.96.4.11\" -t \"devices/lora/807B85902000069B/gpio\" -m \"set 1 1\" -q 1");
+			}
+			else
+			{
+		system ("mosquitto_pub -h  \"10.96.4.11\" -t \"devices/lora/807B85902000069B/gpio\" -m \"set 1 0\" -q 1");
+			}
+		}
 
 	}
   return 0;
 }
+
 
